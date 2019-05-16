e2be9f2f1f3aa85f3a7f99d68f19c3531d6fdae5
Gerson Noboa <gerson.noboa@proekspert.ee>
2019-05-16 23:36:58 +0300

    Added charts test

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-statistics',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
+  isLoading = false;
+  chart = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.loadChart();
+  }
+
+  loadChart() {
+    let data = {
+      datasets: [{
+        data: [10, 20, 30],
+        backgroundColor: [
+          'rgba(255, 99, 132)',
+          'rgba(54, 162, 235)',
+          'rgba(255, 206, 86)'
+        ]
+      }],
+      labels: [
+        'Red',
+        'Blue',
+        'Yellow'
+      ],
+    };
+
+    this.chart = new Chart("canvas", {
+      type: 'doughnut',
+      data: data
+    });
   }
 
 }
